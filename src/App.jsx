eee0a18fa9caed966542a8cf205e118e0a3d3e5c
talperets <tal.perets3@gmail.com>
2023-06-06 13:30:21 +0300

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,8 @@ function App() {
   return (
     <>
       <Menu />
-
       <P5Background />
       <Home />
-      <div>
-        {" "}
-        Icons made by{" "}
-        <a
-          href="https://www.flaticon.com/authors/pixel-perfect"
-          title="Pixel perfect"
-        >
-          {" "}
-          Pixel perfect{" "}
-        </a>{" "}
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com'
-        </a>
-      </div>
     </>
   );
 }
